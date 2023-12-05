db943a0692e2accdf3916993557893e3ff37a4a2
ksbleek <kyle.s.bleeker@gmail.com>
2023-12-05 13:51:40 -0600

    finished user input jdbc exercise

--- src/main/java/dao/MySQLAlbumsDAO.java
+++ src/main/java/dao/MySQLAlbumsDAO.java
@@ -2,9 +2,11 @@ package dao;
 
 import com.mysql.cj.jdbc.Driver;
 import config.Config;
-import models.Quote;
+import models.Album;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MySQLAlbumsDAO {
     // initialize the connection to null so we know whether or not to close it when done
@@ -53,6 +55,135 @@ public class MySQLAlbumsDAO {
             System.out.println("Connection closed.");
         } catch(SQLException e) {
             // ignore this
+        }
+    }
+
+    public List<Album> fetchAlbums() {
+        List<Album> albums = new ArrayList<>();
+        Album album = null;
+        // TODO: write your code here
+
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery("SELECT * FROM albums");
+            while (rs.next()){
+                album = new Album();
+                album.setId(rs.getLong("id"));
+                album.setArtist(rs.getString("artist"));
+                album.setName(rs.getString("name"));
+                album.setReleaseDate(rs.getInt("release_date"));
+                album.setSales(rs.getDouble("sales"));
+                album.setGenre(rs.getString("genre"));
+                albums.add(album);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return albums;
+    }
+
+
+    public Album fetchAlbumById(long id) {
+        Album album = null;
+
+        // TODO: write your code here
+
+        try {
+            PreparedStatement st = connection.prepareStatement("select * from albums " +
+                    " where id = ? ");
+            st.setLong(1, id);
+            ResultSet rs = st.executeQuery();
+            rs.next();
+
+                album = new Album();
+                album.setId(rs.getLong("id"));
+                album.setArtist(rs.getString("artist"));
+                album.setName(rs.getString("name"));
+                album.setReleaseDate(rs.getInt("release_date"));
+                album.setSales(rs.getDouble("sales"));
+                album.setGenre(rs.getString("genre"));
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return album;
+    }
+
+    // Note that insertAlbum should return the id that MySQL creates for the new inserted album record
+    public long insertAlbum(Album album) throws MySQLAlbumsException {
+        long id = 0;
+
+        // TODO: write your code here
+        PreparedStatement st = null;
+        String artist = album.getArtist();
+        String name = album.getName();
+        String release_date = String.valueOf(album.getReleaseDate());
+        String sales = String.valueOf(album.getSales());
+        String genre = album.getGenre();
+
+        try {
+            st = connection.prepareStatement("insert into albums " +
+                    " (artist, name, release_date, sales, genre) " +
+                    " values(?, ?, ?, ?, ?) ", Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, artist);
+            st.setString(2, name);
+            st.setString(3, release_date);
+            st.setString(4, sales);
+            st.setString(5, genre);
+
+            int numInserted = st.executeUpdate();
+
+            ResultSet keys = st.getGeneratedKeys();
+            keys.next();
+
+            id = keys.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return id;
+    }
+
+    public void updateAlbum(Album album) throws MySQLAlbumsException {
+
+        // TODO: write your code here
+        try {
+            PreparedStatement st = connection.prepareStatement("update albums " +
+                    " set artist = ? " +
+                    " , name = ? " +
+                    " , release_date = ? " +
+                    " , sales = ? " +
+                    " , genre = ? " +
+                    " where id = ? ");
+            st.setString(1, album.getArtist());
+            st.setString(2, album.getName());
+            st.setInt(3, album.getReleaseDate());
+            st.setDouble(4, album.getSales());
+            st.setString(5, album.getGenre());
+            st.setLong(6, album.getId());
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAlbumById(long id) throws MySQLAlbumsException {
+
+        // TODO: write your code here
+        PreparedStatement st = null;
+        try {
+            st = connection.prepareStatement("delete from albums " +
+                    " where id = ? ");
+            st.setLong(1, id);
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
--- src/main/java/dao/MySQLAlbumsTest.java
+++ src/main/java/dao/MySQLAlbumsTest.java
@@ -1,5 +1,9 @@
 package dao;
 
+import models.Album;
+
+import java.util.List;
+
 public class MySQLAlbumsTest {
     public static void main(String[] args) {
         MySQLAlbumsDAO albumsDao = new MySQLAlbumsDAO();
@@ -10,6 +14,39 @@ public class MySQLAlbumsTest {
             System.out.println("Using the connection...");
             int numAlbums = albumsDao.getTotalAlbums();
             System.out.println("Total # of album records: " + numAlbums);
+
+            // fetch all albums and print the size
+            // this should equal the above Total # of album records:
+            List<Album> albums = albumsDao.fetchAlbums();
+            System.out.println("Number of album records: " + ((List<?>) albums).size());
+
+            // fetch and print a single album (the album with id 1 in the db)
+            Album album = albumsDao.fetchAlbumById(1L);
+            System.out.println("Album with id 1: " + album);
+
+            // insert a new album
+            Album newAlbum = new Album();
+            newAlbum.setName("Wish You Were Hear");
+            newAlbum.setArtist("Pink Floyd");
+            newAlbum.setReleaseDate(1975);
+            newAlbum.setGenre("Progressive Rock");
+            newAlbum.setSales(9.2);
+            long newId = albumsDao.insertAlbum(newAlbum);
+            System.out.println("Id for new album: " + newId);
+            newAlbum.setId(newId);
+
+            // fix a mistake in the new album
+            newAlbum.setName("Wish You Were Here");
+            albumsDao.updateAlbum(newAlbum);
+
+            // let's confirm that the update worked
+            album = albumsDao.fetchAlbumById(newId);
+            System.out.println("Fixed album: " + album);
+
+            // delete the album
+            albumsDao.deleteAlbumById(newId);
+            System.out.println("Album with id " + newId + " deleted");
+
         } catch(MySQLAlbumsException e) {
             System.out.println(e.getMessage());
         } finally {
